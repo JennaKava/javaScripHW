a7490f179f5069cd509247c6ef072c8525edd6dd
Honey <honey@Yauhenis-MacBook-Pro.local>
2022-07-07 21:25:42 -0700

    Java Script HW update

--- homeWork_3.js
+++ homeWork_3.js
@@ -34,9 +34,9 @@ console.log(`whole 4-word pharse converted to Titalcase -> ${wholePhrase}`);
   * Create abbreviation for any 4-word sentence
   */
 let phrase2 = "do what you love"
-let firstChar = phrase2.substring(0, 1).toUpperCase()
-let secondChar = phrase2.substring(3, 4).toUpperCase()
-let thirdChar = phrase2.substring(8, 9).toUpperCase()
-let fourthChar = phrase2.substring(12, 13).toUpperCase()
-let abbreviation = firstChar.concat(secondChar, thirdChar, fourthChar)
+let firstChar = phrase2.substring(0, 1)
+let secondChar = phrase2.substring(3, 4)
+let thirdChar = phrase2.substring(8, 9)
+let fourthChar = phrase2.substring(12, 13)
+let abbreviation = firstChar.concat(secondChar, thirdChar, fourthChar).toUpperCase()
 console.log(`Abbreviation for the phrase "${phrase2}" is -> ${abbreviation}`);
